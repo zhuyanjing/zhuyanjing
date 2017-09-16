00f701eac539f1713b4857ae84b99d91bbfd0715
zhuyanjing <1482593354@qq.com>
2017-09-16 10:46:00 +0800

    no ff

--- Hello2.java
+++ Hello2.java
@@ -1,7 +1,7 @@
 public class Hello2{
 	public static void main(String  args[]){
 
-		System.out.println("Hello2,no");
-		System.out.println("Hello2,yes");
+		System.out.println("Hello2,no2");
+		System.out.println("Hello2,yes2");
 	}
 }
