646b3309d89da4342f31e507cffd754ca66950f9
zhuyanjing <1482593354@qq.com>
2017-09-16 15:39:45 +0800

    patch  apply

--- Hello2.java
+++ Hello2.java
@@ -1,12 +1,7 @@
 public class Hello2{
 	public static void main(String  args[]){
 
-
-		System.out.println("Hello2,no3");
-		System.out.println("Hello2,yes3");
-
 		System.out.println("Hello2,no2");
-		System.out.println("Hello2,yes233");
-
+		System.out.println("Hello2,yes2");sdf
 	}
 }
